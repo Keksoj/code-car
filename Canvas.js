2daf3db0d8dfd92fba80df54971d2b186bbe4532
Ripeer <Utilisateur@DESKTOP-J000V85>
2021-03-02 10:29:30 +0100

    added_prettierrc

--- Canvas.js
+++ Canvas.js
@@ -1,6 +1,8 @@
+// JavaScript source code
+// Personnalisation du canvas
 
 
 var canvas = document.getElementById("canvas");
 canvas.style.width = '70%';
-canvas.style.height = '70%x';
+canvas.style.height = '70%';
 canvas.style.border = '1px black solid';
