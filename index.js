d33492f5941fc2ed99e56ea74d14473224c5dc53
Emmanuel Bosquet <emmanuel.bosquet@clever-cloud.com>
2021-03-29 15:02:44 +0200

    created canvas ctx

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ var canvas = document.getElementById('canvas');
 canvas.style.width = '70%';
 canvas.style.height = '70%';
 canvas.style.border = '1px black solid';
+
+var ctx = canvas.getContext('2d');
+
+var game = new Game(ctx);
+
+game.start();
